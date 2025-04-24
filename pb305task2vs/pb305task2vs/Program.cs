e1cfff8908818a3861acab0bc59b065e603c36fc
akifnihat <akifst@code.edu.az>
2025-04-24 15:33:22 +0400

    added vscode

---
+++ pb305task2vs/pb305task2vs/Program.cs
@@ -0,0 +1,55 @@
+﻿//Console.WriteLine("Hello, World!");
+#region
+//a)verilmiş ədədin 3-ə bölünüb bölünmədiyini tapan alqoritm
+
+////Console.WriteLine("ededi daxil edin");
+////int input = int.Parse(Console.ReadLine());
+
+////if (input == 0)
+////{
+////    Console.WriteLine("duzgun eded daxil edin.");
+////}
+////else if (input % 3 == 0)
+////{
+////    Console.WriteLine("eded 3e bolunur");
+////}
+////else
+////{
+////    Console.WriteLine("eded 3e bolunmur");
+////}
+#endregion
+
+
+
+#region
+//  b)Verilmiş ədədin 10-dan böyük və 15-dən kiçik olub olmadığını tapan alqoritm
+//Console.WriteLine("ededi daxil edin");
+//int result = int.Parse(Console.ReadLine());
+
+//if (result > 10 && result < 15)
+//{
+//    Console.WriteLine("eded bu araliqdadir");
+//}
+//else
+//{
+//    Console.WriteLine("eded bu araliqda deyil");
+//}
+
+#endregion
+
+#region
+//c)Verilmiş n ədədinin 3-ə və 7-ə bölünüb bölünmədiyini tapan alqoritm qurun
+
+//Console.Write("Bir eded daxil edin: ");
+//int n = int.Parse(Console.ReadLine());
+
+//if (n % 3 == 0 && n % 7 == 0)
+//    Console.WriteLine("Eded hem 3-e, hem de 7-e bolunur.");
+//else if (n % 3 == 0)
+//    Console.WriteLine("Eded yalnız 3-e bolunur.");
+//else if (n % 7 == 0)
+//    Console.WriteLine("Eded yalnız 7-e bolunur.");
+//else
+//    Console.WriteLine("Eded ne 3-e, ne de 7-e bolunur.");
+
+#endregion
